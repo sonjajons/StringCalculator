9edfaacf6e385bedbaa9afd764aeede09f24def0
sonjajons <sonja13@ru.is>
2014-10-16 22:43:46 +0000

    Added a test for the new delimiter

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -35,4 +35,8 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
+	@Test
+    public void testNewDelimiter(){
+    	assertEquals(3, Calculator.add("//;\n1;2"));
+    }    
 }
